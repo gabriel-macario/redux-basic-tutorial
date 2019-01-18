cda7a0e2520c2f345254aad07a38f6bfc4caa688
gabriel-macario <gmacarioweb@gmail.com>
2019-01-18 00:21:53 -0800

    Add store and Provider to src/index

--- src/index.js
+++ src/index.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import todoAppReducer from './redux/reducers';
-
-import {
-    addTodo,
-    toggleTodo,
-    setVisibilityFilter,
-    VisibilityFilters
-  } from './redux/actions'
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
 
 const store = createStore(todoAppReducer);
 
-// Log Initial State
-console.log(store.getState());
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-// Every time state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
-// Dispatch some actions
-store.dispatch(addTodo('Learn about actions'))
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
-store.dispatch(toggleTodo(0))
-store.dispatch(toggleTodo(1))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-// Stop listenint to state updates
-unsubscribe();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
