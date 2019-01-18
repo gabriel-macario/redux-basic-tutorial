2f75c903e76b9b530c095a0fff7578b942592e5c
gabriel-macario <gmacarioweb@gmail.com>
2019-01-18 00:06:41 -0800

    Complete FilterLink container component

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import { setVisibilityFilter } from '../redux/actions';
+import Link from '../components/Link';
 
-const FilterLink = ({ filter }) => (
-    <div>
-        Filler
-    </div>
-)
+const mapStateToProps = (state, ownProps) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    };
+};
+
+const mapDispatchToProps = (state, ownProps) => {
+    return {
+        onClick: () => {
+            dispatchEvent(setVisibilityFilter(ownProps.filter));
+        }
+    };
+};
+
+const FilterLink = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Link);
 
 export default FilterLink;
